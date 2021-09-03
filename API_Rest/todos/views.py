da51bbcce0eecc7a2c3825cb9f7b30c5a39e5420
lmiguelug <luism3872@gmail.com>
2021-09-03 11:58:37 -0400

    incorporating search / filter for 'todos'

--- API_Rest/todos/views.py
+++ API_Rest/todos/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from todos.serializers import TodoSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import filters
 from todos.models import Todo
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 # class CreateTodoAPIView(CreateAPIView):
@@ -27,6 +29,10 @@ class TodosAPIView(ListCreateAPIView):
     
     serializer_class = TodoSerializer
     permission_classes = (IsAuthenticated,)
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+
+    filterset_fields = ['id', 'tittle', 'description', 'is_complete']
+    search_fields = ['id', 'tittle', 'description', 'is_complete']
 
     def perform_create(self, serializer):
         return serializer.save( owner = self.request.user)
